d7dcd5f057ee194cbc6962cf840538017a107ce9
fabianoGDB <doctorfabi.f@gmail.com>
2025-10-07 23:35:14 -0300

    why use scoped in database connections

--- Demos/01 - Inicial/DependencyStore/Program.cs
+++ Demos/01 - Inicial/DependencyStore/Program.cs
@@ -2,12 +2,15 @@ using DependencyStore.Repositories;
 using DependencyStore.Repositories.Contracts;
 using DependencyStore.Services;
 using DependencyStore.Services.Contracts;
+using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+
+builder.Services.AddScoped(x => new SqlConnection("CONN_STR");
 builder.Services.AddTransient<ICostumerRepository, CostumerRepository>();
 builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
 builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
